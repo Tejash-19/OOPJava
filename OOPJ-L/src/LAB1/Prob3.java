2d0af1ce7b9d9b2eebb74f93bafa3f74384c6be8
Tejash Kumar <goal2crpa@gmail.com>
2025-12-11 17:22:18 +0530

    Problem 3

---
+++ OOPJ-L/src/LAB1/Prob3.java
@@ -0,0 +1,45 @@
+package LAB1;
+import java.util.Scanner;
+public class Prob3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day number for the month December 2025: ");
+        int date=sc.nextInt();
+        if(date>31||date<1){
+            System.out.println("Invalid Date");
+        }
+        else {
+            int d = date%7;
+            switch (d) {
+                case 0: {
+                    System.out.println("Sunday");
+                    break;
+                }
+                case 1: {
+                    System.out.println("Monday");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Tuesday");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Wednesday");
+                    break;
+                }
+                case 4: {
+                    System.out.println("Thursday");
+                    break;
+                }
+                case 5: {
+                    System.out.println("Friday");
+                    break;
+                }
+                case 6: {
+                    System.out.println("Saturday");
+                    break;
+                }
+            }
+        }
+    }
+}
