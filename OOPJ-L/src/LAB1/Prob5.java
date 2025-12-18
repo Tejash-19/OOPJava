80ef3c6a3d51bfa722a9cf46470e88af134d032d
Tejash Kumar <goal2crpa@gmail.com>
2025-12-18 14:20:11 +0530

    Update Prob5.java

--- OOPJ-L/src/LAB1/Prob5.java
+++ OOPJ-L/src/LAB1/Prob5.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Prob5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first name: ");
+        System.out.print("Enter first name: ");
         String fname=sc.nextLine();
-        System.out.println("Enter second name: ");
-        String sname=sc.nextLine();
-        System.out.println("Name: "+sname+" "+fname);
+        System.out.print("Enter last name: ");
+        String lname=sc.nextLine();
+        System.out.print("Name: "+lname+" "+fname);
         sc.close();
     }
 }
